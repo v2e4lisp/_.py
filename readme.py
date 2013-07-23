228aa5193de48f947bed5548253a2bdaf34b2089
v2e4lisp <mylastnameisyan@gmail.com>
2013-07-23 11:33:44 +0900

    docs

--- readme.py
+++ readme.py
@@ -2,14 +2,30 @@ import sys
 sys.path.append("/Users/wenjunyan/_py/src")
 sys.path.append("/Users/wenjun.yan/tmp/_.py/src")
 from underscore import _
+import inspect
 import re
 
 def p(x): print(x)
+
+def args(x):
+    s = inspect.getargspec(getattr(_, x))
+    r = ", ".join(s.args[1:])
+    if s.varargs:
+        r = r + ", *" + s.varargs
+    if s.keywords:
+        r = r + ", **" + s.keywords
+    return r
+
+def dl(x):
+    return x + "("+ args(x) + ")"
+
+
 (_(dir(_))
  .filter(lambda x: getattr(_, x).__doc__ and not x.startswith('__'))
- .map(lambda x: "### "+ x + getattr(_, x).__doc__)
- .map(lambda x: x.replace("        ", "").replace("e.g.", "```python\n").replace("type", "\n**type**") + "```\n")
+ .map(lambda x: "### "+ dl(x) + getattr(_, x).__doc__)
+ .map(lambda x: inspect.cleandoc(x).replace("e.g.", "```python\n").replace("type", "\n**type**") + "```\n")
  .map(lambda x: x.replace("@param", "\n**@param**"))
  .map(lambda x: x.replace("@return", "\n**@return**"))
  .each(p))
+
 
