d2ed5f86fd9efeab7f908c4fdbbe23314b6b3df5
v2e4lisp <mylastnameisyan@gmail.com>
2013-07-23 11:09:24 +0900

    docs

--- readme.py
+++ readme.py
@@ -5,10 +5,11 @@ from underscore import _
 import re
 
 def p(x): print(x)
-r = re.compile("        ")
 (_(dir(_))
  .filter(lambda x: getattr(_, x).__doc__ and not x.startswith('__'))
  .map(lambda x: "### "+ x + getattr(_, x).__doc__)
- .map(lambda x: r.sub("", x).replace("e.g.", "```python\n").replace("type", "\n**type**") + "```\n")
+ .map(lambda x: x.replace("        ", "").replace("e.g.", "```python\n").replace("type", "\n**type**") + "```\n")
+ .map(lambda x: x.replace("@param", "\n**@param**"))
+ .map(lambda x: x.replace("@return", "\n**@return**"))
  .each(p))
 
